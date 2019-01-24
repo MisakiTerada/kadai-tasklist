128ecabd0ead103f66ceea0991320cc210983a83
misaki.terada <ikasim0224@gmail.com>
2019-01-24 06:35:08 +0000

    user show

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
 
   resources :tasks;
-
+  
+  get "signup",to: "users#new"
+  resources :user, only: [:index, :show, :new, :create]
+  
 end
